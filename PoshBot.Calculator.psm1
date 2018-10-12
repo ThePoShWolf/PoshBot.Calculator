88b3afccaf86da2fab4c2c52bfa5bb544d5b1cd6
Anthony Howell <anthony@howell-it.com>
2018-10-12 12:17:20 -0700

    Added invoke-colculation

---
+++ PoshBot.Calculator.psm1
@@ -0,0 +1,12 @@
+Function Invoke-Calculation{
+    [CmdletBinding()]
+    [PoshBot.BotCommand(
+        CommandName = 'c'
+    )]
+    Param(
+        [ValidatePattern('^[0-9\+\-\*\/]*$')] #string should only have numbers and the following symbols: + - * /
+        [string]$ToCalc
+    )
+    $scriptBlock = [ScriptBlock]::Create($ToCalc)
+    Write-Output (Invoke-Command $scriptBlock)
+}
