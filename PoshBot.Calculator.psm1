f200c6c6c4b48e9172fad538d53fc1ca9c830f13
Anthony Howell <anthony@howell-it.com>
2018-10-12 15:17:15 -0700

    Now supports parenthesis for realz. Also removes illegal characters.

--- PoshBot.Calculator.psm1
+++ PoshBot.Calculator.psm1
@@ -3,12 +3,24 @@ Function Invoke-Calculation{
     [PoshBot.BotCommand(
         CommandName = 'c'
     )]
-    Param(
+    <#Param(
         [ValidatePattern('^[0-9\+\-\*\/\(\)\.]*$')] #string should only have numbers and the following symbols: + - * / ( ) .
         [string]$ToCalc
-    )
-    $scriptBlock = [ScriptBlock]::Create($ToCalc)
-    Write-Output (Invoke-Command $scriptBlock)
+    )#>
+    # If I do it this way with the $args variable, then I can accept () in the equation.
+    # Otherwise it separates everything to different parameters, which breaks the function.
+    $ToCalc = $args -join ''
+    $regex = '[0-9\+\-\*\/\(\)\.]'
+    $oppositeRegex = '[^0-9\+\-\*\/\(\)\.]'
+    If($ToCalc -match "^$regex*$"){
+        $scriptBlock = [ScriptBlock]::Create($ToCalc)
+        Write-Output (Invoke-Command $scriptBlock)
+    }Else{
+        $illegalCharacters = $ToCalc -replace $regex, ''
+        Write-Output "Illegal characters: '$illegalCharacters' were removed."
+        $scriptBlock = [ScriptBlock]::Create(($ToCalc -replace $oppositeRegex, ''))
+        Write-Output (Invoke-Command $scriptBlock)
+    }
 }
 
 Export-ModuleMember -Function Invoke-Calculation
