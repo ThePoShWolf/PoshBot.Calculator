bde799522d282baae3cd0843a55cd3ecdfbd29f8
2awn <anthony@howell-it.com>
2018-10-12 22:11:20 -0700

    supports ^ as the exponent operator now

--- PoshBot.Calculator.psm1
+++ PoshBot.Calculator.psm1
@@ -10,17 +10,48 @@ Function Invoke-Calculation{
     # If I do it this way with the $args variable, then I can accept () in the equation.
     # Otherwise it separates everything to different parameters, which breaks the function.
     $ToCalc = $args -join ''
-    $regex = '[0-9\+\-\*\/\(\)\.]'
-    $oppositeRegex = '[^0-9\+\-\*\/\(\)\.]'
-    If($ToCalc -match "^$regex*$"){
-        $scriptBlock = [ScriptBlock]::Create($ToCalc)
-        Write-Output (Invoke-Command $scriptBlock)
-    }Else{
+    $regex = '[0-9\+\-\*\/\(\)\.\^]'
+    $oppositeRegex = '[^0-9\+\-\*\/\(\)\.\^]'
+    If($ToCalc -match $oppositeRegex){
         $illegalCharacters = $ToCalc -replace $regex, ''
         Write-Output "Illegal characters: '$illegalCharacters' were removed."
-        $scriptBlock = [ScriptBlock]::Create(($ToCalc -replace $oppositeRegex, ''))
-        Write-Output (Invoke-Command $scriptBlock)
+        $ToCalc = $ToCalc -replace $oppositeRegex, ''
     }
+
+    While($ToCalc -match '\^'){
+        $mathstring = '[math]::pow('
+        $x = ($ToCalc.IndexOf('^') - 1)
+        $y = $x + 2 + $mathstring.Length
+        While($ToCalc[$x] -match '\d' -and $x -gt -1){
+            $x--
+        }
+        If($x -eq -1){
+            $newString = $mathstring + $ToCalc.Replace('^',',')
+        }Elseif(($x -gt -1) -and ($x -lt $ToCalc.length-1)){
+            $newString = ($ToCalc[0..$x] -join '') + $mathstring + ($ToCalc[($x+1)..$ToCalc.length] -join '').Replace('^',',')
+        }Else{ # Invalid usage of '^'
+
+        }
+        While(($newstring[$y] -match '\d') -and ($y -lt $newstring.Length-1)){
+            $y++
+        }
+        If($y -lt $newstring.Length){
+            $ToCalc = ($newString[0..$y] -join '') + ')' + ($newString[($y+1)..$newstring.length] -join '')
+        }
+    }
+    $Scriptblock = [scriptblock]::Create($ToCalc)
+    Write-Output (Invoke-Command $Scriptblock)
 }
 
-Export-ModuleMember -Function Invoke-Calculation
+Function Get-SquareRoot{
+    [CmdletBinding()]
+    [PoshBot.BotCommand(
+        CommandName = 'sqrt'
+    )]
+    Param(
+        [int]$num
+    )
+    Write-Output ([math]::Sqrt($num))
+}
+
+Export-ModuleMember -Function Invoke-Calculation, Get-SquareRoot
